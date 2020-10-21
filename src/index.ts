01f519aade0553ecdf5ddfd7331355a535067dab
Alex Schneider <me@schneider.ax>
2020-10-21 23:44:06 +0700

    Rework using code from outmatch

--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,190 @@
-interface Options {
-  separator?: string
+function escapeRegExpChar(char: string) {
+  if (
+    char === '-' ||
+    char === '^' ||
+    char === '$' ||
+    char === '+' ||
+    char === '.' ||
+    char === '(' ||
+    char === ')' ||
+    char === '|' ||
+    char === '[' ||
+    char === ']' ||
+    char === '{' ||
+    char === '}' ||
+    char === '*' ||
+    char === '?' ||
+    char === '\\'
+  ) {
+    return `\\${char}`
+  } else {
+    return char
+  }
 }
 
 function escapeRegExpString(str: string) {
-  return str.replace(/[-^$+.()|[\]{}]/g, '\\$&')
-}
-
-function trimRight(str: string, substr: string) {
-  if (str.substr(-substr.length) === substr) {
-    return str.substr(0, str.length - substr.length)
+  let result = ''
+  for (let i = 0; i < str.length; i++) {
+    result += escapeRegExpChar(str[i])
   }
-
-  return str
+  return result
 }
 
-function buildPatternWithSeparators(pattern: string, separator: string) {
-  let segments = pattern.split(separator)
-  return segments.reduce((result, segment, i) => {
-    if (segment === '**') {
-      if (i === 0) {
-        return `(.*${separator})?`
-      } else if (i === segments.length - 1) {
-        return `${trimRight(result, separator)}(${separator}.*)?`
-      }
-      return `${trimRight(result, separator)}(${separator}.*)?${separator}`
-    }
-
-    // The order of these replacements is important because the latter contains a ?
-    segment = segment
-      .replace(/(?<!\\)\?/g, `(?!${separator}).`)
-      .replace(/(?<!\\)\*/g, `(((?!${separator}).)*|)`)
-
-    if (i < segments.length - 1) {
-      return `${result}${segment}${separator}`
-    }
-
-    return `${result}${segment}`
-  }, '')
-}
-
-function buildRegExpPattern(pattern: string | string[], separator?: string): string {
+function compile(pattern: string | string[], separator?: string | boolean): string {
   if (Array.isArray(pattern)) {
-    let regExpPatterns = pattern.map((p) => `^${buildRegExpPattern(p, separator)}$`)
-    return `(${regExpPatterns.join('|')})`
+    let regExpPatterns = pattern.map((p) => `^${compile(p, separator)}$`)
+    return `(?:${regExpPatterns.join('|')})`
   }
 
-  if (pattern === '**') {
-    return '^.*$'
-  }
+  let separatorSplitter = ''
+  let separatorMatcher = ''
+  let wildcard = '.'
 
-  let regExpPattern: string
+  if (separator === true) {
+    // In this case forward slashes in patterns match both forward and backslashes in samples
+    separatorSplitter = '/'
+    separatorMatcher = '[/\\\\]'
+    wildcard = '[^/\\\\]'
+  } else if (separator) {
+    separatorSplitter = separator
+    separatorMatcher = escapeRegExpString(separatorSplitter)
 
-  if (!separator) {
-    regExpPattern = escapeRegExpString(pattern)
-      .replace(/(?<!\\)\?/g, '.')
-      .replace(/(?<!\\)\*/g, '.*')
-  } else if (pattern === '*') {
-    regExpPattern = `((?!${escapeRegExpString(separator)}).)*`
+    if (separatorMatcher.length > 1) {
+      separatorMatcher = `(?:${separatorMatcher})`
+      wildcard = `((?!${separatorMatcher}).)`
+    } else {
+      wildcard = `[^${separatorMatcher}]`
+    }
   } else {
-    regExpPattern = buildPatternWithSeparators(
-      escapeRegExpString(pattern),
-      escapeRegExpString(separator)
+    wildcard = '.'
+  }
+
+  // When a separator is explicitly specified in a pattern, it must match _one or more_
+  // separators in a sample, so we use quantifiers. When a pattern doesn't have a trailing
+  // separator, a sample can still optionally have them, so we use different quantifiers
+  // depending on the index of a segment.
+  let requiredSeparator = separator ? `${separatorMatcher}+?` : ''
+  let optionalSeparator = separator ? `${separatorMatcher}*?` : ''
+
+  let segments = separator ? pattern.split(separatorSplitter) : [pattern]
+  let result = ''
+
+  for (let s = 0; s < segments.length; s++) {
+    let segment = segments[s]
+    let nextSegment = segments[s + 1]
+    let currentSeparator = ''
+
+    if (!segment && s > 0) {
+      continue
+    }
+
+    if (separator) {
+      if (s === segments.length - 1) {
+        currentSeparator = optionalSeparator
+      } else if (nextSegment !== '**') {
+        currentSeparator = requiredSeparator
+      } else {
+        currentSeparator = ''
+      }
+    }
+
+    if (separator && segment === '**') {
+      if (currentSeparator) {
+        result += s === 0 ? '' : currentSeparator
+        result += `(?:${wildcard}*?${currentSeparator})*?`
+      }
+      continue
+    }
+
+    for (let c = 0; c < segment.length; c++) {
+      let char = segment[c]
+
+      if (char === '\\') {
+        if (c < segment.length - 1) {
+          result += escapeRegExpChar(segment[c + 1])
+          c++
+        }
+      } else if (char === '?') {
+        result += wildcard
+      } else if (char === '*') {
+        result += `${wildcard}*?`
+      } else {
+        result += escapeRegExpChar(char)
+      }
+    }
+
+    result += currentSeparator
+  }
+
+  return result
+}
+
+interface isMatch {
+  /**
+   * Tests if a sample string matches the pattern(s)
+   *
+   * ```js
+   * isMatch('foo') //=> true
+   * ```
+   */
+  (sample: string): boolean
+
+  /** The compiled regular expression */
+  regexp: RegExp
+
+  /** The original pattern or array of patterns that was used to compile the RegExp */
+  pattern: string | string[]
+
+  /** The separator that was used to split the pattern into segments */
+  separator?: string | boolean
+}
+
+function isMatch(regexp: RegExp, sample: string) {
+  if (typeof sample !== 'string') {
+    throw new TypeError(`Sample must be a string, but ${typeof sample} given`)
+  }
+
+  return regexp.test(sample)
+}
+
+/**
+ * Compiles one or more glob patterns into a RegExp and returns an isMatch function.
+ * The isMatch function takes a sample string as its only argument and returns true
+ * if the string matches the pattern(s).
+ *
+ * ```js
+ * wildcardMatch('src/*.js')('src/index.js') //=> true
+ * ```
+ *
+ * ```js
+ * const isMatch = wildcardMatch('*.example.com', '.')
+ * isMatch('foo.example.com') //=> true
+ * isMatch('foo.bar.com') //=> false
+ * ```
+ */
+function wildcardMatch(pattern: string | string[], separator?: string | boolean) {
+  if (typeof pattern !== 'string' && !Array.isArray(pattern)) {
+    throw new TypeError(
+      `Pattern must be a string or an array of strings, but ${typeof pattern} given`
     )
   }
 
-  return `^${regExpPattern}$`
-}
-
-function wildcardMatch(pattern: string | string[], separator?: string | Options) {
-  let options: Options = typeof separator === 'object' ? separator : { separator }
-  let regexpPattern = buildRegExpPattern(pattern, options.separator)
-  let regExp = new RegExp(regexpPattern) as RegExp & {
-    pattern?: string | string[]
-    options: Options
+  if (
+    typeof separator !== 'undefined' &&
+    typeof separator !== 'string' &&
+    typeof separator !== 'boolean'
+  ) {
+    throw new TypeError(`Separator must be a string, but ${typeof separator} given`)
   }
-  regExp.pattern = pattern
-  regExp.options = options
-  return regExp
+
+  let regexpPattern = compile(pattern, separator)
+  let regexp = new RegExp(`^${regexpPattern}$`)
+
+  let fn = isMatch.bind(null, regexp) as isMatch
+  fn.pattern = pattern
+  fn.regexp = regexp
+  fn.separator = separator
+  return fn
 }
 
 export default wildcardMatch
